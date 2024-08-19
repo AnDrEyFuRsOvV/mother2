369ed561185e8cffc63af0a0f9c64a9d4e387540
AnDrEyFuRsOvV <fursov.andrei.3097@gmail.com>
2024-08-20 01:26:58 +0300

    add new code

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         dots: true,
         prevArrow: '<button type="button" class="slick-left"><img src="././images/arrow-1.png" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-right"><img src="././images/arrow-2.png" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-right"><img src="././images/arrow-2.png" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 1460,
+              settings: {
+                arrows: false,
+              }
+            },
+            {
+              breakpoint: 1310,
+              settings: {
+                centerMode: false,
+                arrows: false,
+              }
+            }
+        ]
     });
   });
